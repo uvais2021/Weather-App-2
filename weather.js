5156a84d67649272cffa0a13d254ccf61afbfbda
Uvais A <uvais2021@gmail.com>
2025-07-01 21:02:36 +0530

    Weather app done

---
+++ weather.js
@@ -0,0 +1,71 @@
+// const apiKey = "9828d008e9f6feb8100ecf72c33cfa94";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// const cityname = document.querySelector('#cityInput');
+// const searchbtn = document.querySelector('.searchBtn')
+
+// async function getWeather(city) {
+//     try {
+//         const response = await fetch(apiUrl + city + `&appid=${apiKey}&units=metric`);
+//         const data = await response.json();
+
+//         if (response.ok) {
+//             console.log(data);
+//             document.querySelector('.result').innerHTML = `
+//                 City: ${data.name} <br>
+//                 Temp: ${data.main.temp} °C <br>
+//                 Weather: ${data.weather[0].description}
+//             `;
+//         } else {
+//             document.querySelector('.result').innerHTML = `Error: ${data.message}`;
+//         }
+//     } catch (error) {
+//         console.error("Fetch error:", error);
+//         document.querySelector('.result').innerHTML = "Something went wrong!";
+//     }
+// }
+
+// searchbtn.addEventListener("click", () => {
+//     const city = cityname.value.trim();
+//     if (cityname === '') {
+//         alert('Plz type ant name')
+//     } else {
+//         getWeather(cityname.value);
+//     }
+// });
+
+const apiKey = "9828d008e9f6feb8100ecf72c33cfa94";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+const cityname = document.querySelector('#cityInput');
+const searchbtn = document.querySelector('.searchBtn');
+
+async function getWeather(city) {
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}&units=metric`);
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+            document.querySelector('.result').innerHTML = `
+                City: ${data.name} <br>
+                Temp: ${data.main.temp} °C <br>
+                Weather: ${data.weather[0].description}
+            `;
+        } else {
+            document.querySelector('.result').innerHTML = `Error: ${data.message}`;
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+        document.querySelector('.result').innerHTML = "Something went wrong!";
+    }
+}
+
+searchbtn.addEventListener('click', () => {
+    const city = cityname.value.trim();
+    if (city === '') {
+        alert('Please type a city name');
+    } else {
+        getWeather(city);
+    }
+});
